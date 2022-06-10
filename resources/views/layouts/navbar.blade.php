0e46284ebb93827961f91e8d79dee061fe9da347
dip.ghosh <dip.ghosh@shohoz.com>
2022-06-10 23:58:09 +0600

    blade integration complete

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,83 @@
+<header class="header-style-1">
+
+    <!-- ============================================== TOP MENU ============================================== -->
+    <div class="top-bar animate-dropdown">
+        <div class="container">
+            <div class="header-top-inner">
+                <div class="cnt-account">
+                    <ul class="list-unstyled">
+                        <li class="myaccount"><a href="#"><span>My Account</span></a></li>
+                        <li class="wishlist"><a href="#"><span>Wishlist</span></a></li>
+                        <li class="header_cart hidden-xs"><a href="#"><span>My Cart</span></a></li>
+                        <li class="check"><a href="#"><span>Checkout</span></a></li>
+                        <li class="login"><a href="#"><span>Login</span></a></li>
+                    </ul>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+    <!-- ============================================== TOP MENU : END ============================================== -->
+    <div class="main-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
+                    <div class="logo"> <a href="home.html"> <img src="assets/images/logo.png" alt="logo"> </a> </div>
+                </div>
+                <div class="col-lg-7 col-md-6 col-sm-8 col-xs-12 top-search-holder">
+                    <!-- ============================================================= SEARCH AREA ============================================================= -->
+                    <div class="search-area">
+                        <form>
+                            <div class="control-group">
+                                <ul class="categories-filter animate-dropdown">
+                                    <li class="dropdown"> <a class="dropdown-toggle"  data-toggle="dropdown" href="category.html">Categories <b class="caret"></b></a>
+                                        <ul class="dropdown-menu" role="menu" >
+                                            <li class="menu-header">Computer</li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Clothing</a></li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Electronics</a></li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Shoes</a></li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Watches</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                                <input class="search-field" placeholder="Search here..." />
+                                <a class="search-button" href="#" ></a> </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12 animate-dropdown top-cart-row">
+                    <!-- ============================================================= QUICK CART ============================================================= -->
+                    <div class="dropdown dropdown-cart"> <a href="#" class="lnk-cart" data-toggle="dropdown">
+                            <div class="items-cart-inner">
+                                <div class="basket">
+                                    <div class="basket-item-count"><span class="count">2</span></div>
+                                    <div class="total-price-basket"> <span class="lbl">Shopping Cart</span> <span class="value">$4580</span> </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- ============================================================= QUICK CART : END ============================================================= -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ============================================== NAVBAR ============================================== -->
+    <div class="header-nav animate-dropdown">
+        <div class="container">
+            <div class="yamm navbar navbar-default" role="navigation">
+                <div class="navbar-header">
+                    <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+                        <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                </div>
+                <div class="nav-bg-class">
+                    <div class="navbar-collapse collapse" id="mc-horizontal-menu-collapse">
+                        <div class="nav-outer">
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
+});
+
+Route::get('/products/details',function () {
+    return view('products.product-details');
 });
